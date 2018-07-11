470f9f96005e7635323413bf0be382dd42164b57
Reed Xia <huaqin2005@gmail.com>
2018-07-11 17:56:06 +0800

    work on models desigin...

--- meetup/models.py
+++ meetup/models.py
@@ -1,16 +1,20 @@
 from django.db import models
 
 
-class Event(models.Model):
+class Topic(models.Model):
 
-    subject = models.CharField(max_length=100)
-    status = models.BooleanField()
-    address = models.CharField(max_length=150)
-    start_time = models.DateTimeField('events time')
+    title = models.CharField(max_length=100)
+    speaker = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
     create_time = models.DateTimeField(auto_now=True)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)  
     
 
     def __str__(self):
-        return self.subject
+        return self.title
 
+class Event(models.Model):
+
+    name = models.CharField(max_length=100)
+    topic_list = 
 
